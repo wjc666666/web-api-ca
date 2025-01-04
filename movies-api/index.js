81dd262b1440a2754fe44bb48f3ab4f4d465212f
wjc666666 <2101079563@qq.com>
2025-01-04 23:51:20 +0800

    add router

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,23 +1,29 @@
-import express from 'express';
+import express from 'express'; 
 import dotenv from 'dotenv';
 import cors from 'cors';
-import usersRouter from './api/users';
-import './db';
-import defaultErrHandler from './errHandler';
-import moviesRouter from './api/movies';   //import movies router
-
+import usersRouter from './api/users/index.js';
+import moviesRouter from './api/movies/index.js';
+import favouritesRouter from './api/favourites/favourites.js';
+import reviewsRouter from './api/reviews/reviews.js';
+import preferencesRouter from './api/preferences/preferences.js'; 
+import './db/index.js';
+import defaultErrHandler from './errHandler/index.js';
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT; 
+const port = process.env.PORT || 8080; 
 
 app.use(cors());
 app.use(express.json());
+
 app.use('/api/users', usersRouter);
+app.use('/api/movies', moviesRouter);
+app.use('/api/favourites', favouritesRouter);
+app.use('/api/reviews', reviewsRouter);
+app.use('/api/preferences', preferencesRouter); 
 app.use(defaultErrHandler);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
 });
-app.use('/api/movies', moviesRouter); //ADD THIS BEFORE THE DEFAULT ERROR HANDLER.
